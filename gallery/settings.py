d6217afef38f181a343936ddf67ca1f407d9c338
Alex Kiprop <kipropalex59@gmail.com>
2019-11-11 15:29:44 +0300

    modified the files and upload new images

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pix.apps.PixConfig',
+    'pix',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
