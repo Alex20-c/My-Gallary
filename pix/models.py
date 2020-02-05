5c8b0b2f03ae303c1bff44fc149f3876a76b5b81
Alex Kiprop <kipropalex59@gmail.com>
2020-02-05 14:43:01 +0300

    make major update to the program

--- pix/models.py
+++ pix/models.py
@@ -3,23 +3,48 @@ import datetime as dt
 
 
 # Create your models here.
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-
+class Editor(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
+    
+    def __str__(self):
+        return self.first_name
+    
+    def save_editor(self):
+        self.save()
+    
+    class Meta:
+        ordering = ['first_name']
+        verbose_name = 'Editor'
+        verbose_name_plural = 'Editors'
+class tags(models.Model):
+    name = models.CharField(max_length= 20)
+    
     def __str__(self):
         return self.name
-
+    
+    class Meta:
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    
     def save_category(self):
         self.save()
-
+        
+    def __str__(self):
+        return self.name
+        
 class Location(models.Model):
     name = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
-
     def save_location(self):
         self.save()
+        
+    def __str__(self):
+        return self.name
 
 
 class Image(models.Model):
--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/alex/Desktop/my-gallery/virtual"
+set -gx VIRTUAL_ENV "/home/alex/Desktop/Projects/my-gallery/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
